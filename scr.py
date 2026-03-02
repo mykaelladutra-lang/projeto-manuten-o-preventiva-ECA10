546750b26fcfffc85e9ca9d3fb6ade5bcede6187
mykaelladutra-lang <mykaelladutra@gmail.com>
2026-03-02 00:33:40 -0300

    Update scr.py

--- scr.py
+++ scr.py
@@ -8,19 +8,4 @@ def gerar_dados(qtd_registros=100):
     dados = []
     tempo_inicial = datetime.now()
 
-    for i in range(qtd_registros):
-        registro = {
-            "timestamp": tempo_inicial + timedelta(minutes=i),
-            "temperatura": round(random.uniform(60, 95), 2),
-            "vibracao": round(random.uniform(1.5, 5.0), 2),
-            "corrente": round(random.uniform(3.5, 7.0), 2)
-        }
-        dados.append(registro)
-
-    df = pd.DataFrame(dados)
-    df.to_csv("dados/dados_simulados.csv", index=False)
-    print("Dados gerados com sucesso!")
-
-if __name__ == "__main__":
-    gerar_dados()
 
